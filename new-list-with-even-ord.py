f3b0a328b9b32496c61bce38cf06edb92c6a9fc2
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-05-26 17:21:47 +0530

    easier commit

--- new-list-with-even-ord.py
+++ new-list-with-even-ord.py
@@ -5,11 +5,11 @@ a = [10, 20, 30, 40, 50, 23, 27, 33]
 b = [37, 27, 73, 12, 22, 26, 20, 30]
 c = []
 
-for i in range(len(a)):
-    if a[i] % 2 != 0:
-        c.append(a[i])
-for f in range(len(b)):
-    if b[f] % 2 == 0:
-        c.append(b[f])
+for i in a:
+    if i % 2 == 1:
+        c.append(i)
+for f in b:
+    if f % 2 == 0:
+        c.append(f)
 
 print(c)
