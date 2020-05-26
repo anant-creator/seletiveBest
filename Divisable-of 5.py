16e3037b59e5ab9f29d301c532b3b462f9afe473
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-05-26 14:20:34 +0530

    Divisable of 3 and 5 within range

---
+++ Divisable-of 5.py
@@ -0,0 +1,5 @@
+''' Question 6: Given a list of numbers, Iterate it and print only those numbers which are divisible of 5 '''
+
+for i in range(101):
+    if i % 3 == 0 and i % 5== 0:
+        print(i)
