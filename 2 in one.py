aab2e4aa612601c00b427278ba1bb12e4c491d32
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-05-27 09:32:49 +0530

    2 in 0ne created

---
+++ 2 in one.py
@@ -0,0 +1,6 @@
+''' Exercise Question 1: Accept two numbers from the user and calculate multiplication '''
+
+a = input("Enter a number:- )
+b = input("Enter b number:- )
+print(a * b)
+print("My", "Name", "is", "Anant", sep="**")
