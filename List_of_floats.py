32292c4bcc4093172f0a3a4529316143f04d9130
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-05-27 22:29:21 +0530

    List of floats created

---
+++ List_of_floats.py
@@ -0,0 +1,7 @@
+''' Exercise Question 5: Accept list of 5 float numbers as a input from user '''
+
+a = []
+for i in range(6):
+	b = float(input("Enter a value for list:- "))
+	a.append(b)
+	print(a)
