4c45d8ed895b50205eccdc41db21719b1616093f
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-05-27 22:12:28 +0530

    Two digits after decimal

---
+++ Two-digits-after-decimal.py
@@ -0,0 +1,5 @@
+''' Exercise Question 4: Display float number with 2 decimal places using print() '''
+a = float(input("Enter a float:- "))
+b = float(input("Enter a float:- "))
+flt = a * b
+print("%.2f" %flt)
