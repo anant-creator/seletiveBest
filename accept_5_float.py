a934b4d311d9b0bd5410dbe545defeb8e466557e
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-02 00:07:33 +0530

    5 input floats

---
+++ accept_5_float.py
@@ -0,0 +1,7 @@
+''' Exercise Question 5: Accept list of 5 float numbers as a input from user '''
+
+a = []
+for i in range(5):
+    b = float(input("Enter your input here:- "))
+    a.append(b)
+print(a)
