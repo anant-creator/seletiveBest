b705a97ffb0d028c4409860283dc633d440b9f2d
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-02 01:11:21 +0530

    .format used successfully

---
+++ format_using.py
@@ -0,0 +1,6 @@
+''' Exercise Question 8: You have following data display it using string.format() method as per the following condition '''
+
+a = 1000
+b = 3
+c = 450
+print("I have {} dollars so I can buy {} footballs for {} dollars.".format(a, b, c))
