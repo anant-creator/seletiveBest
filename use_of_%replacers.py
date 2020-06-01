5cc3bdbd9a6e8a7d2497bc11bb5717e43421fa45
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-02 00:02:26 +0530

    use of %f %s %d created

---
+++ use_of_%replacers.py
@@ -0,0 +1,8 @@
+''' Exercise Question 4: Display float number with 2 decimal places using print() and use of %d and %s '''
+
+a = 12.9999999
+print(type(a))
+print("This is a float %.2f" % (a))
+b = "Anant"
+c = 22
+print("I am a good boy my name is %s and my age is %d" % (b, c))
