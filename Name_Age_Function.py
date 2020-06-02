03388e29893a8b63319f15b55236836eda7523f6
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-02 15:23:31 +0530

    name age printing function

---
+++ Name_Age_Function.py
@@ -0,0 +1,6 @@
+''' Exercise Question 1: Create a function that can accept two arguments name and age and print its value '''
+
+def person(name, age):
+    print("Hello! my name is %s and my age is %d" % (name, age))
+
+person("Anant", 22)
