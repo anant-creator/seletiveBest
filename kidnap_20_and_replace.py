764dac625ec31fe3e99cdb931100b5da1602eb64
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-24 20:19:29 +0530

    20 kidnapped and replaced with 200

---
+++ kidnap_20_and_replace.py
@@ -0,0 +1,6 @@
+''' Exercise Question 9: Given a Python list, find value 20 in the list, and if it is present, replace it with 200. Only update the first occurrence of a value '''
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+a = list1.index(20)
+list1[a] = 200
+print(list1)
