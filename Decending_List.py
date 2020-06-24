279dac4275acb649caa39a08fb0123a4cb279fb4
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-24 14:06:31 +0530

    Decending list created

---
+++ Decending_List.py
@@ -0,0 +1,4 @@
+''' Exercise Question 1: Given a Python list you should be able to display Python list in the following order '''
+
+aLsit = [100, 200, 300, 400, 500]
+print(sorted(aLsit, reverse=True))
