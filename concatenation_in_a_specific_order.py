832df220929b665d0c7a6abce147270eceff4cae
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-24 16:36:44 +0530

    concatenation complete

---
+++ concatenation_in_a_specific_order.py
@@ -0,0 +1,8 @@
+''' Exercise Question 4: Concatenate two lists in the following order '''
+
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+
+for i in list1:
+    for j in list2:
+        print(i + j)
