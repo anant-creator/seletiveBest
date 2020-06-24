a6eddffdfa0ab21fbd8b0280be6912c744e53d6e
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-24 20:27:08 +0530

    Dictionary created

---
+++ create_a_dictionary.py
@@ -0,0 +1,7 @@
+''' dictionary exercise 1: Below are the two lists convert it into the dictionary '''
+
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+dicky = dict(zip(keys, values))
+print(dicky)
