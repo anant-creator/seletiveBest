c6c657233f254fe176421f9d284438e23cb8ef0c
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-06-24 16:29:14 +0530

    List of squared items created

---
+++ list_of_a_squared_items.py
@@ -0,0 +1,8 @@
+''' Exercise Question 3: Given a Python list. Turn every item of a list into its square '''
+
+aList = [1, 2, 3, 4, 5, 6, 7]
+bList = []
+for i in aList:
+    bList.append(i*i)
+
+print(bList)
